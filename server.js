1416da4134b27d3a47b55a16a48ec41d31836d64
owaisj <jamil.owais@gmail.com>
2019-07-22 10:16:46 -0500

    Linting, prettier, testing environment setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const db = require('./models');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+/* eslint-disable no-console */
+db.sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+});
