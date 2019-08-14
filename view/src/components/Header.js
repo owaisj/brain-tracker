90e9dab3968d1f6eab9c3bba3d784bd5418d0db3
owaisj <jamil.owais@gmail.com>
2019-08-13 20:04:22 -0500

    Fixed Navbar combining rbx with bulma classes for valid DOM nesting

--- view/src/components/Header.js
+++ view/src/components/Header.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import { Hero, Container, Title } from 'rbx';
+import React, { Fragment } from 'react';
+import { Hero, Container, Title, Navbar } from 'rbx';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBrain } from '@fortawesome/free-solid-svg-icons';
+
+const CustomNav = () => {
+  const items = ['Your Blog', 'Your Tracker', 'About the App', 'Sign-up`'];
+
+  // TODO: Add an icon
+  return (
+    <Navbar fixed="top">
+      <Navbar.Brand>
+        <Link className="navbar-item is-hoverable nav-link" to="/">
+          <FontAwesomeIcon icon={faBrain} />
+        </Link>
+      </Navbar.Brand>
+
+      {items.map((item, i) => (
+        <Link key={i} className="navbar-item is-hoverable nav-link" to="/">
+          {item}
+        </Link>
+      ))}
+    </Navbar>
+  );
+};
 
 export default function Header() {
   return (
-    <Hero color="success">
-      <Hero.Body>
-        <Container>
-          <Title>
-            <Link className="title-link" to="/">
-              Brain Tracker
-            </Link>
-          </Title>
-        </Container>
-      </Hero.Body>
-    </Hero>
+    <Fragment>
+      <CustomNav />
+      <Hero color="success">
+        <Hero.Body>
+          <Container>
+            <Title>
+              <Link className="title-link" to="/">
+                Brain Tracker
+              </Link>
+            </Title>
+          </Container>
+        </Hero.Body>
+      </Hero>
+    </Fragment>
   );
 }
