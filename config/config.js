70e6d1bd0517b336e9b3e79d80336567cb200b06
owaisj <jamil.owais@gmail.com>
2019-08-16 12:34:47 -0500

    Config tweaks for heroku

--- config/config.js
+++ config/config.js
@@ -23,7 +23,7 @@ module.exports = {
     use_env_variable: 'DATABASE_URL',
     username: 'root',
     password: null,
-    database: 'database_production',
+    database: 'mentalhealthdb_prod',
     host: '127.0.0.1',
     dialect: 'postgres',
     operatorsAliases: false
