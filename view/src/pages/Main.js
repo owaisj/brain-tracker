532ca7eb84ecce0ef7e4c2e1d9a12414593646d6
owaisj <jamil.owais@gmail.com>
2019-08-16 23:56:01 -0500

    Styling for page body with home page content

--- view/src/pages/Main.js
+++ view/src/pages/Main.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Section, Container, Tile, Title, Content, Notification } from 'rbx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBed, faPencilAlt, faSun } from '@fortawesome/free-solid-svg-icons';
 import Sidebar from '../components/Sidebar';
 
 export default function(props) {
@@ -21,6 +23,10 @@ export default function(props) {
               <Title>Features</Title>
               <Content />
               <Title subtitle size={6}>
+                <FontAwesomeIcon
+                  style={{ marginRight: '0.5em' }}
+                  icon={faPencilAlt}
+                />{' '}
                 Journal:{' '}
                 <Content as="span" size="small">
                   Record your thoughts and feelings.
@@ -28,12 +34,20 @@ export default function(props) {
               </Title>
 
               <Title subtitle size={6}>
+                <FontAwesomeIcon
+                  style={{ marginRight: '0.5em' }}
+                  icon={faSun}
+                />{' '}
                 Mood:{' '}
                 <Content as="span" size="small">
                   Grade how you're feeling from 1-10.
                 </Content>
               </Title>
               <Title subtitle size={6}>
+                <FontAwesomeIcon
+                  style={{ marginRight: '0.5em' }}
+                  icon={faBed}
+                />{' '}
                 Sleep:{' '}
                 <Content as="span" size="small">
                   Log how many hours of sleep you've gotten.
